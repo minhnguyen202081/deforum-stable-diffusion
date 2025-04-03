0a61281c8ed8c866fc8b69c13aa6cac42a741da7
Minh Nguyen <minhxp100@hotmail.com>
2025-04-03 12:31:43 +0700

    aa

--- helpers/model_load.py
+++ helpers/model_load.py
@@ -176,7 +176,7 @@ def load_model(root, load_on_run_all=True, check_sha256=True, map_location="cuda
         "lexica-aperture-v3-5": {
             'sha256': '7c72418d6a51f5c752d00faa53c014f52fe0ea2f2dda1661620a50dffa96d0f9',
             'url': 'https://huggingface.co/openskyml/lexica-aperture-v3-5/blob/main/Lexica-Aperture-V3.5.ckpt',
-            'requires_login': False,
+            'requires_login': True,
             },
     }
 
